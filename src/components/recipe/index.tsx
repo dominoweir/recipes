6f235d2d003b99e40839fc7459273c5584c70fac
Domino Weir <dominoweir@gmail.com>
2020-08-02 15:00:25 -0400

    add emotion and material ui

--- src/components/recipe/index.tsx
+++ src/components/recipe/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
 const Recipe: React.FunctionComponent = () => {
-  return <></>;
+  return <Container>
+    <Typography variant="h2">Recipe Name</Typography>
+    <Typography variant="subtitle1">Servings</Typography>
+  </Container>;
 }
 
 export default Recipe;
