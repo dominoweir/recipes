aa8bb288f0533e3699217119f8f0f5d56865bdc0
Domino Weir <dominoweir@gmail.com>
2020-08-02 14:40:24 -0400

    initial commit

---
+++ src/components/category/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Category: React.FunctionComponent = () => {
+  return <></>;
+}
+
+export default Category;
---
+++ src/components/editor/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Editor: React.FunctionComponent = () => {
+  return <></>;
+}
+
+export default Editor;
